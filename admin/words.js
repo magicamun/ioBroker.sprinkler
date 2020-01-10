8821e0ae69d4187878f60ad44919de57704d6d35
Lutz Thielemann <Lutz.Thielemann@arcor.de>
2020-01-10 18:30:52 +0100

    initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"sprinkler adapter settings": {
+		"en": "Adapter settings for sprinkler",
+		"de": "Adaptereinstellungen für sprinkler",
+		"ru": "Настройки адаптера для sprinkler",
+		"pt": "Configurações do adaptador para sprinkler",
+		"nl": "Adapterinstellingen voor sprinkler",
+		"fr": "Paramètres d'adaptateur pour sprinkler",
+		"it": "Impostazioni dell'adattatore per sprinkler",
+		"es": "Ajustes del adaptador para sprinkler",
+		"pl": "Ustawienia adaptera dla sprinkler",
+		"zh-cn": "sprinkler的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
